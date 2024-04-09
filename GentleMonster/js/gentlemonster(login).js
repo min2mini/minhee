e18f628b671d2f02d802b573046e2583a4fc00dc
min2mini <159765032+min2mini@users.noreply.github.com>
2024-04-09 15:52:37 +0900

    delete
    삭제

--- GentleMonster/js/gentlemonster(login).js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-        // 로고 SVG -> LOGIN 화면에서 BLACK으로 색상변경
-        $('.h_svg').attr('fill', 'black')
-        // 검색 SVG -> LOGIN 화면에서 BLACK으로 색상변경
-        $('.search_svg').attr('style', 'fill:#000')
-
-       
-})
